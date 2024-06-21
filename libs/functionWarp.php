0b6fc2391e8a9540c1242ed487115ebdfa3a7fa3
Andreas Röne <andreas@digital-first.com>
2024-06-21 12:20:01 +0200

    Change function handling

--- libs/functionWarp.php
+++ libs/functionWarp.php
@@ -2,9 +2,6 @@
 
 function sapiFunctionWarp($functionName,$para) {
     global $apiUrl,$apiVersion,$apiKey;
-    echo("----------------------\n");
-    echo($command . "\n");
-    echo("----------------------\n");
     $para = substr($para,0,-1);
     $requestUrl = 'https://' . $apiUrl . '/' . $apiVersion . '/' . $functionName . '/?' . $para;
     $result = slCallPostUrl($requestUrl,$apiKey,'');
