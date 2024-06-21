8741cae50ac8bbead0a50d797d8cc1f01ede75b3
Andreas Röne <andreas@digital-first.com>
2024-06-21 11:48:08 +0200

    Change function handling

--- libs/api.php
+++ libs/api.php
@@ -4,17 +4,9 @@ error_reporting(0);
 
 
 if (file_exists('config/conf.php')) {
-  require_once('config/conf.php');
-  require_once('libs/https.php');
-
-  if (file_exists('libs/commands/' . $command . '.php')) {
-      require_once('libs/commands/' . $command . '.php');
-      $phpCommand = 'sapi_' . $command;
-      $result = $phpCommand($para);
-  } else {
-      echo("function not found\n");
-  }
-
+    require_once('config/conf.php');
+    require_once('libs/https.php');
+    $result = sapiFunctionWarp($command, $para);
 } else {
-  echo("No configuration with API key has been created yet. Please complete the installation first.");
+    echo("No configuration with API key has been created yet. Please complete the installation first.");
 }
