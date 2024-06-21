9e3248a4e7a4094f2e2ea98515765f7aa21ffcce
Andreas Röne <andreas@digital-first.com>
2024-06-21 09:51:04 +0200

    Add new funciton getforwardingdomains

--- libs/commands/getforwardingdomains.php
+++ libs/commands/getforwardingdomains.php
@@ -5,5 +5,8 @@ function sapi_getforwardingdomains($para) {
     $para = substr($para,0,-1);
     $requestUrl = 'https://' . $apiUrl . '/' . $apiVersion . '/getforwardingdomains/?' . $para;
     $result = slCallPostUrl($requestUrl,$apiKey,'');
+    echo(".........\n");
+    print_r($result);
+    echo(".........\n");
     return json_decode($result,TRUE);
 }
