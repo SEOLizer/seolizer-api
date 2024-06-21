cb080c65454232a41212a5d499b1b72b49842f05
Andreas Röne <andreas@digital-first.com>
2024-06-21 18:42:16 +0200

    Change function handling

--- libs/functionWarp.php
+++ libs/functionWarp.php
@@ -1,10 +1,9 @@
 <?php
 
-function sapiFunctionWarp($functionName,$para) {
-    global $apiUrl,$apiVersion,$apiKey;
+
+function sapiFunctionWarp($apiUrl,$apiVersion,$functionName,$para,$apiKey) {
     $para = substr($para,0,-1);
     $requestUrl = 'https://' . $apiUrl . '/' . $apiVersion . '/' . $functionName . '/?' . $para;
     $result = slCallPostUrl($requestUrl,$apiKey,'');
     return json_decode($result,TRUE);
-
 }
