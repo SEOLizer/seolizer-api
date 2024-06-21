7e4d945af5d510f951388b54145fc397965ff3ca
Andreas Röne <andreas@digital-first.com>
2024-06-21 10:04:45 +0200

    Add new funciton getforwardingdomains

--- libs/https.php
+++ libs/https.php
@@ -1,14 +1,15 @@
 <?php
 
-function slCallPostUrl($url,$key,$data) {
-  $headers[] = 'Authorization: Bearer ' . $key;
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL,$url);
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  $server_output = curl_exec($ch);
-  curl_close ($ch);
-  return $server_output;
+function slCallPostUrl($url, $key, $data) {
+    $headers[] = 'Authorization: Bearer ' . $key;
+    $headers[] = 'Accept: application/json';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $server_output = curl_exec($ch);
+    curl_close($ch);
+    return $server_output;
 }
